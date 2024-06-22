469da95434b6ecabd5ffa2612c12245cb87df3b8
mrsyafapri <rizkisyafapri@gmail.com>
2024-06-22 17:48:42 +0700

    fix: make check admin utils more efficient

--- utils/checkAdmin.js
+++ utils/checkAdmin.js
@@ -3,23 +3,23 @@ require("dotenv").config();
 
 const checkAdminStatus = async (authToken) => {
     const perusahaanServicesUrls = process.env.PERUSAHAAN_SERVICES_URLS.split(',');
+    const requests = perusahaanServicesUrls.map(url => axios.get(`${url}/api/v1/employee/profile`, {
+        headers: { Authorization: `Bearer ${authToken}` }
+    }));
 
-    for (const url of perusahaanServicesUrls) {
-        try {
-            const response = await axios.get(`${url}/api/v1/employee/profile`, {
-                headers: { Authorization: `Bearer ${authToken}` }
-            });
-
+    try {
+        const responses = await Promise.all(requests);
+        for (const response of responses) {
             const employee = response.data.data;
             if (employee.isAdmin) {
                 return true;
             }
-        } catch (error) {
-            console.error(`Error fetching from ${url}`);
         }
+        return false;
+    } catch (error) {
+        console.error('Error fetching admin status');
+        return false;
     }
-
-    return false;
 };
 
-module.exports = { checkAdminStatus };
+module.exports = checkAdminStatus;
